7d522bfe82049d0a7ddc03f80466199bf52d3f7d
Devesh Sharma <deveshsharma372002@gmail.com>
2025-02-09 14:47:01 +0530

    updated components

---
+++ Episode 2/vite-project/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import "./Navbar.css";
+const Navbar= ()=>{
+    return(
+        <>
+        <div id="navbar">
+      <div><img src="https://do6gp1uxl3luu.cloudfront.net/banner+and+logos/icon.webp"  height="50px" width="50px" alt="logo" /></div>
+      <div>
+        <ul id="nav-links">
+          <li>Home</li>
+          <li>About-Us</li>
+          <li>Projects</li>
+          <li>Contact us</li>
+        </ul>
+      </div>
+
+    </div>
+        
+        </>
+    )
+}
+export default Navbar;
